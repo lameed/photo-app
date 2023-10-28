acd1f85a1354962e71af2c7c870100737b0185ee
lameed <lameedkunle@yahoo.com>
2023-10-28 23:13:42 +0100

    add payment

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,7 +1,7 @@
 class Payment < ApplicationRecord
 
-	attr_accessor :card_number, :card_cvv, :card_expires_month, :card_expires_year
-	belongs_to :users
+	attr_accessor :card_number, :card_cvc, :card_expires_month, :card_expires_year
+	belongs_to :user
 
 	def self.month_options
 		Date::MONTHNAMES.compact.each_with_index.map{ |name, i| ["#{i+1} - #{name}", i+1]}
--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => ENV['pk_test_51O5uuhE9yu6fjCtdFuZgP4noIcYZKACNooz0ckptCGsI9SevSVnY4aXuKtWP3AtlgQ6caTvall0ZE6X6tHFfSS1b00AyiEc6V6'],
+  :secret_key      => ENV['sk_test_51O5uuhE9yu6fjCtdFT8D4JszT2gLIQFjcDbErLrm377AOn2LdukTlA9L6NVqFcvfzitkT5r03FYyC5UcNnJvqhsg00iVwtaBLt']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
